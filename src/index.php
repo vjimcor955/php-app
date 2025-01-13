09fbc2d64f68d28215c95890878aedc93899f58c
vjimcor955 <vjimcor955@g.educaand.es>
2025-01-13 11:51:18 +0100

    input to lowercase and title

--- src/index.php
+++ src/index.php
@@ -1,3 +1,5 @@
+<h1>Buscador nombres PHP</h1>
+
 <form method="post">
     <input type="text" name="search" placeholder="Introduce una letra">
     <input type="submit" name="submit" value="Buscar">
@@ -13,15 +15,15 @@ if(isset($_POST['submit'])){
     // if name contains the letter entered by user add it to the result array
     $result = array();
     foreach($names as $name){
-        if(strpos($name, $letter) !== false){
-            array_push($result, $name);
-        }
+      if (strpos(strtolower($name), strtolower($letter)) !== false) {
+        array_push($result, $name);
+      }
     }
     // display the result
     if (empty($result)){
-      echo "<h3>Ningun nombre contiene la letra $letter</h3>";
+      echo "<h2>Ningun nombre contiene la letra $letter</h2>";
     } else {
-      echo "<h3>Nombres que contienen la letra $letter:</h3>";
+      echo "<h2>Nombres que contienen la letra $letter:</h2>";
       foreach($result as $name){
         echo "- " . $name . "<br>";
       }
