bb8ab2de0f42f7bb0b0ac5be629f333e7cffc392
vjimcor955 <vjimcor955@g.educaand.es>
2025-01-13 11:58:14 +0100

    update names

--- src/index.php
+++ src/index.php
@@ -6,25 +6,45 @@
 </form>
 
 <?php
+
 // Names array
-$names = array("Adriana", "Belen", "Carmen", "Diana", "Elena", "Fernando", "Gabriel", "Hugo", "Isabel", "Javier", "Karla", "Luis", "Maria", "Natalia", "Oscar", "Pablo", "Queralt", "Rosa", "Sara", "Tomas");
+$names = array(
+  "Víctor Jiménez Corada", 
+  "Yeray Almoguera González",  
+  "Álvaro Caro Fernández", 
+  "Carlos Cordero Moreno", 
+  "Alejandro Díaz Barea", 
+  "Santiago Domínguez Gómez", 
+  "Lucía Espinosa Sánchez", 
+  "Alejandro González Benítez", 
+  "Ángel Martínez Sánchez", 
+  "Pablo Olvera Colino", 
+  "Gonzalo Pulido Sánchez", 
+  "Francisco Javier Rodríguez Acosta", 
+  "Nicolás Ruiz Ruiz", 
+  "Félix Sánchez González", 
+  "Alejandro Seoane Martínez", 
+  "Rafael Tocino Batista", 
+  "Israel Valderrama García", 
+  "Isaac Vallet Colchero"
+);
 
 // search bar
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $letter = $_POST['search'];
     // if name contains the letter entered by user add it to the result array
     $result = array();
-    foreach($names as $name){
+    foreach($names as $name) {
       if (strpos(strtolower($name), strtolower($letter)) !== false) {
         array_push($result, $name);
       }
     }
     // display the result
-    if (empty($result)){
-      echo "<h2>Ningun nombre contiene la letra $letter</h2>";
+    if (empty($result)) {
+      echo "<h2>Ningun nombre contiene la letra $letter.</h2>";
     } else {
       echo "<h2>Nombres que contienen la letra $letter:</h2>";
-      foreach($result as $name){
+      foreach ($result as $name) {
         echo "- " . $name . "<br>";
       }
     }
